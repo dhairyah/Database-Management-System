6c8cc6455e26566dd72be4c71c6de658f15c72e9
Raj Soni <34932185+Ewaolx@users.noreply.github.com>
2018-02-19 15:02:56 -0500

    Update Main.java
    Main Edited- Projection Commit Part 2

--- src/edu/buffalo/www/cse4562/Main.java
+++ src/edu/buffalo/www/cse4562/Main.java
@@ -1,22 +1,19 @@
-package edu.buffalo.www.cse4562;
-import org.apache.commons.csv.*;
+/*package edu.buffalo.www.cse4562;
 import net.sf.jsqlparser.expression.*;
 import net.sf.jsqlparser.statement.*;
 import net.sf.jsqlparser.parser.CCJSqlParser.*;
 import net.sf.jsqlparser.schema.Table;
 import net.sf.jsqlparser.statement.select.*;
-import net.sf.jsqlparser.statement.select.AllColumns.*;
+import net.sf.jsqlparser.statement.select.*;
 import net.sf.jsqlparser.statement.select.SelectBody.*;
 import net.sf.jsqlparser.statement.create.table.*;
-//import net.sf.jsqlparser.statement.create.table.*;
+import net.sf.jsqlparser.statement.create.table.*;
 import java.lang.Object;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.io.*;
 import java.sql.SQLException;
 import java.util.*;
 import net.sf.jsqlparser.parser.*;
-/*public class Main   {
+public class f   {
 	public static void main(String[] args) throws ParseException, SQLException {
 		System.out.println("Hello, World");
 		Reader input =  new StringReader("CREATE TABLE R(A int,B int, C int);SELECT A ,B FROM  R,E as  Iso  WHERE R.A=5 ");
@@ -30,7 +27,7 @@ import net.sf.jsqlparser.parser.*;
 				//System.out.println(select);
 			    PlainSelect sv=(PlainSelect)select.getSelectBody();                        ;
 		//		System.out.println("1:"+sv.getFromItem().getAlias());
-				BinaryExpression tt=(BinaryExpression)sv.getWhere();*/
+				BinaryExpression tt=(BinaryExpression)sv.getWhere();
 				
 		//		System.out.println(tt.getLeftExpression());
 				
@@ -87,12 +84,19 @@ import net.sf.jsqlparser.parser.*;
 		
 	}
 }*/
-/*package edu.buffalo.www.cse4562;
+package edu.buffalo.www.cse4562;
  
+import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
- 
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+
 import net.sf.jsqlparser.expression.Expression;
 import net.sf.jsqlparser.parser.CCJSqlParser;
 import net.sf.jsqlparser.parser.ParseException;
@@ -102,28 +106,68 @@ import net.sf.jsqlparser.statement.select.FromItem;
 import net.sf.jsqlparser.statement.select.PlainSelect;
 import net.sf.jsqlparser.statement.select.Select;
 import net.sf.jsqlparser.statement.select.SelectBody;
-import net.sf.jsqlparser.statement.select.SelectItem;*/
+import net.sf.jsqlparser.statement.select.SelectItem;
  
-public class Main 
-{
-	public static Tuple tup= new Tuple();
-	 //siz=0;
-	public static RelTreeObj createTree(PlainSelect query)
+public class Main {
+	
+	static CreateTable create;
+	
+	private static void ParseTree(RelTreeObj leafnode) throws IOException
 	{
+		
+		Scan table = (Scan)leafnode.getOperator();
+		Tuple tupleobj = new Tuple();
+		RelTreeObj parentnode = null;
+		int printflag = 1;
+				
+		Reader reader = Files.newBufferedReader(Paths.get("src\\"+table.fromitem+".csv"));
+		CSVParser parser = CSVParser.parse(reader, CSVFormat.DEFAULT.withDelimiter('|').withIgnoreHeaderCase().withTrim());
+		
+		
+		for (CSVRecord tupple : parser.getRecords()) 
+		{
+			tupleobj.record = tupple;
+			tupleobj.table = create;
+			parentnode = leafnode.retParent();
+			printflag = 1;
+			
+			
+			while(parentnode != null)
+			{
+				
+				if(parentnode.operator.api(tupleobj)==true)
+				{
+				 parentnode = parentnode.retParent();
+				}
+				else
+				{
+					printflag = 0;
+					break;
+				}
+			}
+			
+			if(printflag == 1)
+			{
+				System.out.println("Print the tupple : "+tupleobj.record.toString());
+			}
+			
+			
+			
+		}
+	}
+	
+	
+	
+	public static RelTreeObj createTree(PlainSelect query) {
 		RelTreeObj parent = null;
 		RelTreeObj leaf = null;
 		List<SelectItem> selItem = query.getSelectItems();
-		//AllColumns all=AllColumns(selItem.);
-		System.out.println("PRP:"+selItem.get(0));
-		
 		if(!selItem.isEmpty()) {
 			RelationalAlgebra op = new Projection();
 			Projection op1= (Projection)op;
 			op1.projection = selItem;
-			System.out.println("FDsf:::::::::::::");
-		    op1.api(tup);
 			op= (RelationalAlgebra)op1 ;
-			RelTreeObj child= new RelTreeObj(op);
+			RelTreeObj child = new RelTreeObj(op);
 			parent = child;
 		}
 		Expression exp = query.getWhere();
@@ -150,77 +194,45 @@ public class Main
 		
 		return leaf;
 	}
-	
-	
-	public static void main(String[] args) throws ParseException, IOException
-	{
+	public static void main(String[] args) throws ParseException {
 		System.out.println("Hello, World");
-		int siz=0;
+		
 		RelTreeObj leaf = null;
-		Reader input = new StringReader("CREATE TABLE R (A int, B date, C int );SELECT A,C FROM R");
+		Reader input = new StringReader("create table R(c1 integer, c2 integer);SELECT a from R where c1=1");
 		CCJSqlParser parser = new CCJSqlParser(input);
 		Statement statement = parser.Statement();
-		while(statement != null)
-		{
-			if(statement instanceof Select) 
-			{
+		while(statement != null) {
+			if(statement instanceof Select) {
 				Select select = (Select) statement;
-				//CreateTable ct=(CreateTable)statement;
-				//System.out.println("def::"+ct.getColumnDefinitions());
 				SelectBody body = select.getSelectBody();
 				if(body instanceof PlainSelect)
 				{
 					PlainSelect plain = (PlainSelect)body;
 					leaf = createTree(plain);
-					System.out.println("Child detail : " + leaf.retParent().getOperator().getClass());
-					//leaf.getParent().getData().api(t);
-					int k = 0;
+					
+					try 
+					{
+						ParseTree(leaf);
+					} 
+					catch (IOException e) 
+					{
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
 					
 				}
 				int i = 0;
 			}
-			else if(statement instanceof CreateTable)
-			{
-				CreateTable create = (CreateTable)statement;
-				Index ii=new Index();
-				tup.table=create;
-				//ii=(Index)create.getIndexes();
-				//System.out.println(ii.getColumnsNames());
-				//ColumnDefinition cc= (ColumnDefinition)statement;
-				//ColumnDefinition cdd= (ColumnDefinition)statement;
-				System.out.println("GEy"+create.getColumnDefinitions());
-				siz=create.getColumnDefinitions().size();
-				System.out.println(siz);
+			else if(statement instanceof CreateTable) {
+				create = (CreateTable)statement;
 				int k = 0;
 			}
+			
+			
+			
  
 			statement = parser.Statement();
 		}
-		 
-		//Reader reader = Files.newBufferedReader(Paths.get("D:\\Eclipse\\DB\\DB_project\\CSE4562SP18\\R.csv"));
-		//CSVParser parser1 = CSVParser.parse(reader, CSVFormat.DEFAULT.withDelimiter('|').withIgnoreHeaderCase().withTrim());
- 
- 
-		 /*for (CSVRecord csvRecord : parser1.getRecords()) {
-             // Accessing values by the names assigned to each column
- 
- 
-			 String col;
-             
-			 int colNumber =siz;
-			for(int i=0;i<colNumber;i++)
-			 {
- 
-				 col = csvRecord.get(i);
- 
-				 System.out.print(" "+col);
-			 }
-			 System.out.println("");
-         }*/
-		 System.out.println(leaf.getOperator().getClass());
-		 Scan rr=(Scan) leaf.getOperator();
-		 System.out.println(rr.fromitem);
-		 System.out.println(leaf.retParent().getOperator().getClass());	
-		}
+	}
 }
-
