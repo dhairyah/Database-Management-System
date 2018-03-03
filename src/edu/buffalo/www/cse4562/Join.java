92774d0bd5c31103083cd5ed1d1819a83785564a
Prajin Jonchhe <prajinjo@buffalo.edu>
2018-03-03 17:18:48 -0500

    Added Join class

---
+++ src/edu/buffalo/www/cse4562/Join.java
@@ -0,0 +1,14 @@
+package edu.buffalo.www.cse4562;
+
+import java.sql.SQLException;
+
+public class Join implements RelationalAlgebra{
+
+	private RelationalAlgebra node1, node2;
+	@Override
+	public boolean api(Tuple tupleobj) throws SQLException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
