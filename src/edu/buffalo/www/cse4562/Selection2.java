d8b3233f465bc2b06e59eafdbfd5d3dc2b10fcd5
Prajin Jonchhe <prajinjo@buffalo.edu>
2018-03-31 19:29:06 -0400

    Added new class Selection2

---
+++ src/edu/buffalo/www/cse4562/Selection2.java
@@ -0,0 +1,78 @@
+package edu.buffalo.www.cse4562;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import net.sf.jsqlparser.eval.Eval;
+import net.sf.jsqlparser.expression.BooleanValue;
+import net.sf.jsqlparser.expression.Expression;
+import net.sf.jsqlparser.expression.PrimitiveValue;
+import net.sf.jsqlparser.expression.operators.relational.ExpressionList;
+import net.sf.jsqlparser.expression.operators.relational.InExpression;
+import net.sf.jsqlparser.expression.operators.relational.ItemsList;
+import net.sf.jsqlparser.schema.Column;
+import net.sf.jsqlparser.statement.select.SubSelect;
+
+public class Selection2 extends RelationalAlgebra2{
+	
+	public Expression expression;	
+
+	@Override
+	boolean api(Tuple tupleobj) throws SQLException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	Tuple retNext() throws SQLException {
+		Tuple tupleobj = leftChild.retNext();
+
+		Eval eval = new Eval() {
+			@Override
+			public PrimitiveValue eval(Column arg0) throws SQLException {
+				int index = tupleobj.colNames.indexOf(arg0);
+				if(index == -1)
+				{
+					int size = tupleobj.colNames.size();
+					for(int it = 0; it < size; it++)
+					{
+						if((arg0.getTable().getName().equalsIgnoreCase(tupleobj.colNames.get(it).getTable().getAlias())) && 
+								(arg0.getColumnName().equalsIgnoreCase(tupleobj.colNames.get(it).getColumnName())))
+						{
+							index = it;
+							break;
+						}
+					}
+				}
+				return tupleobj.tuple.get(index);
+			}
+
+		};
+		//System.out.println("exp:"+expression);	  
+		PrimitiveValue type = eval.eval(expression);
+		if(type.toBool() == true)
+		{
+			return tupleobj;
+		}
+		else
+		{
+			Tuple tp = this.retNext();
+			return tp;
+		}
+	}
+
+	@Override
+	List<Column> open() throws IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	void close() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
