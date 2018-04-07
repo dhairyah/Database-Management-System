5423d43dd5542f87d396c636738a5867c840b229
Prajin Jonchhe <prajinjo@buffalo.edu>
2018-04-07 19:02:09 -0400

    Modified CompareOverride and OrderBy to handle 2 orderby index

--- src/edu/buffalo/www/cse4562/CompareOverride.java
+++ src/edu/buffalo/www/cse4562/CompareOverride.java
@@ -1,101 +1,219 @@
 package edu.buffalo.www.cse4562;
- 
+
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Comparator;
- 
+
 import net.sf.jsqlparser.eval.Eval;
-import net.sf.jsqlparser.expression.LongValue;
 import net.sf.jsqlparser.expression.PrimitiveValue;
 import net.sf.jsqlparser.expression.PrimitiveValue.InvalidPrimitive;
 import net.sf.jsqlparser.expression.StringValue;
+import net.sf.jsqlparser.expression.operators.relational.EqualsTo;
 import net.sf.jsqlparser.expression.operators.relational.GreaterThan;
 import net.sf.jsqlparser.schema.Column;
- 
+
 public class CompareOverride implements Comparator<ArrayList<PrimitiveValue>> {
-	  int nn;
-	  boolean isAsc;
- 
- 
-	    public CompareOverride(int nn, boolean isAsc) {
-	        this.nn = nn;
-	        this.isAsc = isAsc;
- 
-	    } 
+	int orderByIndex_1, orderByIndex_2;
+	boolean isAsc;
+
+
+	public CompareOverride(int index1, int index2, boolean isAsc) {
+		this.orderByIndex_1 = index1;
+		this.orderByIndex_2 = index2;
+		this.isAsc = isAsc;
+
+	} 
 	@Override
- 
+
 	public int compare(ArrayList<PrimitiveValue> arg0, ArrayList<PrimitiveValue> arg1) {
-		PrimitiveValue v1=arg0.get(nn);
-		PrimitiveValue v2=arg1.get(nn);
-		/*long vv1=0;
-		long vv2=0;
-		if(v1 instanceof LongValue)
-		{
-			 vv1=((LongValue)v1).toLong();
-			 vv2=((LongValue)v2).toLong();
-		}*/
-		if(v1 instanceof StringValue)
-		{
-			String temp_v1 = v1.toString();
-			String temp_v2 = v2.toString();
-			int result = temp_v1.compareTo(temp_v2);
-			if(isAsc)
-			{
-				return result;
-			}
-			else
-			{
-				return result*(-1);
-			}
-		}
+		PrimitiveValue v1=arg0.get(orderByIndex_1);
+		PrimitiveValue v2=arg1.get(orderByIndex_1);
+		String temp_v1, temp_v2;
+		int result_int;
+		PrimitiveValue result_prim = null;
+		Boolean r = null;
+
 		Eval eval = new Eval() {
- 
+
 			@Override
 			public PrimitiveValue eval(Column arg0) throws SQLException {
 				return null;
 			}};
-		PrimitiveValue result = null;
- 
-	    try {
-			result = eval.eval( new GreaterThan(v1,v2));
-		} catch (SQLException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
- 
-		Boolean r = null;
-		try {
-			r = result.toBool();
-		} catch (InvalidPrimitive e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		if(isAsc)
-		{
-			if(r==false)
-		   {
-			   return -1;
-		   }
-		   else
-		   {
-			   return 1;
-		   }
-		}
-		else
-		{
-			if(r==false)
-		   {
-			   return 1;
-		   }
-		   else
-		   {
-			   return -1;
-		   }
-		}
- 
- 
- 
+
+			if(v1 instanceof StringValue)
+			{
+				temp_v1 = v1.toString();
+				temp_v2 = v2.toString();
+				result_int = temp_v1.compareTo(temp_v2);
+				if(result_int == 0 && orderByIndex_2 != -1)
+				{
+					v1=arg0.get(orderByIndex_2);
+					v2=arg1.get(orderByIndex_2);
+					if(v1 instanceof StringValue)
+					{
+						temp_v1 = v1.toString();
+						temp_v2 = v2.toString();
+						result_int = temp_v1.compareTo(temp_v2);
+						if(isAsc)
+						{
+							return result_int;
+						}
+						else
+						{
+							return result_int * (-1);
+						}
+					}
+					else
+					{
+						try {
+							result_prim = eval.eval( new GreaterThan(v1,v2));
+						} catch (SQLException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+						try {
+							r = result_prim.toBool();
+						} catch (InvalidPrimitive e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						if(isAsc)
+						{
+							if(r == false)
+							{
+								return -1;
+							}
+							else
+							{
+								return 1;
+							}
+						}
+						else
+						{
+							if(r == false)
+							{
+								return 1;
+							}
+							else
+							{
+								return -1;
+							}
+						}
+					}
+				}
+				if(isAsc)
+				{
+					return result_int;
+				}
+				else
+				{
+					return result_int * (-1);
+				}
+			}
+
+			try {
+				result_prim = eval.eval( new EqualsTo(v1,v2));
+				try {
+					r = result_prim.toBool();
+				} catch (InvalidPrimitive e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				if(r == true && orderByIndex_2 != -1)
+				{
+					v1=arg0.get(orderByIndex_2);
+					v2=arg1.get(orderByIndex_2);
+					if(v1 instanceof StringValue)
+					{
+						temp_v1 = v1.toString();
+						temp_v2 = v2.toString();
+						result_int = temp_v1.compareTo(temp_v2);
+						if(isAsc)
+						{
+							return result_int;
+						}
+						else
+						{
+							return result_int * (-1);
+						}
+					}
+					else
+					{
+						try {
+							result_prim = eval.eval( new GreaterThan(v1,v2));
+						} catch (SQLException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+
+						try {
+							r = result_prim.toBool();
+						} catch (InvalidPrimitive e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						if(isAsc)
+						{
+							if(r == false)
+							{
+								return -1;
+							}
+							else
+							{
+								return 1;
+							}
+						}
+						else
+						{
+							if(r == false)
+							{
+								return 1;
+							}
+							else
+							{
+								return -1;
+							}
+						}
+					}
+
+				}
+				else
+				{
+					result_prim = eval.eval( new GreaterThan(v1,v2));
+				}
+			} 
+			catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+
+			try {
+				r = result_prim.toBool();
+			} catch (InvalidPrimitive e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			if(isAsc)
+			{
+				if(r==false)
+				{
+					return -1;
+				}
+				else
+				{
+					return 1;
+				}
+			}
+			else
+			{
+				if(r==false)
+				{
+					return 1;
+				}
+				else
+				{
+					return -1;
+				}
+			}
 	}
- 
- 
 }
--- src/edu/buffalo/www/cse4562/OrderBy.java
+++ src/edu/buffalo/www/cse4562/OrderBy.java
@@ -2,27 +2,19 @@ package edu.buffalo.www.cse4562;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
-import net.sf.jsqlparser.eval.Eval;
-import net.sf.jsqlparser.expression.Expression;
-import net.sf.jsqlparser.expression.LongValue;
 import net.sf.jsqlparser.expression.PrimitiveValue;
 import net.sf.jsqlparser.expression.PrimitiveValue.InvalidPrimitive;
-import net.sf.jsqlparser.expression.operators.relational.GreaterThan;
-import net.sf.jsqlparser.expression.operators.relational.GreaterThanEquals;
 import net.sf.jsqlparser.schema.Column;
-import net.sf.jsqlparser.schema.PrimitiveType;
 import net.sf.jsqlparser.statement.select.OrderByElement;
 
 public class OrderBy extends Tuple implements RelationalAlgebra
 {
 	ArrayList<ArrayList<PrimitiveValue>> otable = new ArrayList<ArrayList<PrimitiveValue>>();
 
-	int orderByIndex, tai =0;
+	int orderByIndex_1, orderByIndex_2, tai =0;
 	List<OrderByElement> element; 
 	Tuple ne;
 	public String subQuery_alias;
@@ -31,7 +23,11 @@ public class OrderBy extends Tuple implements RelationalAlgebra
 
 		
 		otable.add(new ArrayList<PrimitiveValue>());
-		orderByIndex=tupleobj.colNames.indexOf((Column) element.get(0).getExpression()); 
+		orderByIndex_1 = tupleobj.colNames.indexOf((Column) element.get(0).getExpression());
+		if(element.size() > 1)
+			orderByIndex_2 = tupleobj.colNames.indexOf((Column) element.get(1).getExpression());
+		else
+			orderByIndex_2 = -1;
 
 		for(int i = 0; i < tupleobj.tuple.size(); i++)
 		{
@@ -47,7 +43,7 @@ public class OrderBy extends Tuple implements RelationalAlgebra
          int c=0;
          //System.out.println("c:"+c);
          //System.out.println("orde:"+element.get(0));
-		 Collections.sort(otable, new CompareOverride(orderByIndex,element.get(0).isAsc()));
+		 Collections.sort(otable, new CompareOverride(orderByIndex_1, orderByIndex_2, element.get(0).isAsc()));
 		 
 		 for(int i=0;i<otable.size();i++)
 		 {
