11660ed00b556b13b2a5d4e26d9ce1c147344523
Dhairya Hardikbhai Desai <32210048+DesaiDhairya@users.noreply.github.com>
2018-04-13 00:09:38 -0400

    Reverting Selection2 changes

--- src/edu/buffalo/www/cse4562/Selection2.java
+++ src/edu/buffalo/www/cse4562/Selection2.java
@@ -27,7 +27,6 @@ public class Selection2 extends RelationalAlgebra2{
 		{
 			return null;
 		}
-
 		Eval eval = new Eval() {
 			@Override
 			public PrimitiveValue eval(Column arg0) throws SQLException {
@@ -47,7 +46,6 @@ public class Selection2 extends RelationalAlgebra2{
 				}
 				return tupleobj.tuple.get(index);
 			}
-
 		};
 		//System.out.println("exp:"+expression);	  
 		PrimitiveValue type = eval.eval(expression);
@@ -62,7 +60,6 @@ public class Selection2 extends RelationalAlgebra2{
 		}
 		*/
 		tupleobj = leftChild.retNext();
-
 		Eval eval = new Eval() {
 			@Override
 			public PrimitiveValue eval(Column arg0) throws SQLException {
@@ -86,22 +83,15 @@ public class Selection2 extends RelationalAlgebra2{
 		};
 		while(tupleobj!=null)
 		{
-			if(expression!=null)
+			//System.out.println("exp:"+expression);	  
+			PrimitiveValue type = eval.eval(expression);
+			if(type.toBool() == true)
 			{
-				//System.out.println("exp:"+expression);	  
-				PrimitiveValue type = eval.eval(expression);
-				if(type.toBool() == true)
-				{
-					return tupleobj;
-				}
-				else
-				{
-					tupleobj = leftChild.retNext();
-				}
+				return tupleobj;
 			}
 			else
 			{
-				return tupleobj;
+				tupleobj = leftChild.retNext();
 			}
 		}
 		return null;
