fca11d5058fff09f48806143777036f178acf4bc
Dhairya Hardikbhai Desai <32210048+DesaiDhairya@users.noreply.github.com>
2018-04-13 15:13:35 -0400

    Trying with java primitive sums

--- src/edu/buffalo/www/cse4562/Aggregate2.java
+++ src/edu/buffalo/www/cse4562/Aggregate2.java
@@ -336,6 +336,9 @@ public class Aggregate2 extends RelationalAlgebra2 {
 	List<Integer> groupByIndex = new ArrayList<Integer>();
 	List<Integer> functionIndex = new ArrayList<Integer>(); 
 	HashMap<String,Tuple> hashAggr;
+	HashMap<String,List<Object>> hashAggrValues;
+	List<Object> aggrValues = new ArrayList<Object>();
+	List<Integer> aggrTypes = new ArrayList<Integer>(); // 0 for long and 1 for double
 	HashMap<String,Integer> aggrKeyCnt;
 //	HashMap<String,  Long> hashSum=new HashMap<>();
 	Iterator<String> hashItr;
@@ -385,10 +388,28 @@ public class Aggregate2 extends RelationalAlgebra2 {
 		{
 			if(init==0)
 			{
-				Tuple childTuple;
+				Tuple childTuple = leftChild.retNext();
 				hashAggr=new HashMap<String, Tuple>();
+				hashAggrValues = new HashMap<String, List<Object>>();
+				
+				for(int i=0;i<aggrFunctions.size();i++)
+				{
+					PrimitiveValue val = childTuple.tuple.get(functionIndex.get(i));
+					if(val instanceof LongValue)
+					{
+						aggrValues.add(0);
+						aggrTypes.add(0);
+					}
+					else if(val instanceof DoubleValue)
+					{
+						aggrValues.add(0.0);
+						aggrTypes.add(1);
+					}
+				}
+				
 				aggrKeyCnt = new HashMap<String, Integer>();
-				while((childTuple=leftChild.retNext())!=null)
+				int aggrIndex;
+				while(childTuple!=null)
 				{
 					for(int i=0;i<groupByIndex.size();i++)
 					{
@@ -397,57 +418,58 @@ public class Aggregate2 extends RelationalAlgebra2 {
 					
 					if(hashAggr.containsKey(groupByColVals))
 					{
-						
-						computeAllStreamAggr(hashAggr.get(groupByColVals),childTuple);
-						hashAggr.put(groupByColVals, childTuple);
+						List<Object> retAggr = new ArrayList<Object>();
+						//computeAllStreamAggr(hashAggr.get(groupByColVals),childTuple);
+						//hashAggr.put(groupByColVals, childTuple);
 						aggrKeyCnt.put(groupByColVals, aggrKeyCnt.get(groupByColVals)+1);
+						//aggrValues.clear();
+						retAggr.addAll(hashAggrValues.get(groupByColVals)); 
+						for(int i=0;i<aggrFunctions.size();i++)
+						{
+							aggrIndex= functionIndex.get(i);
+							if(aggrTypes.get(i)==0)
+							{
+								long longVal = (long)retAggr.get(i) + childTuple.tuple.get(aggrIndex).toLong();
+								retAggr.set(i, longVal);
+							}
+							else
+							{
+								double doubleVal = ((double) retAggr.get(i)) + childTuple.tuple.get(aggrIndex).toDouble();
+								retAggr.set(i, doubleVal);
+							}
+						}
+						hashAggrValues.put(groupByColVals, retAggr);
 						
 					}
 					else	
 					{
+						List<Object> retAggr = new ArrayList<Object>();
 						hashAggr.put(groupByColVals, childTuple);
 						aggrKeyCnt.put(groupByColVals, 1);
+
+						for(int i=0;i<aggrFunctions.size();i++)
+						{
+							aggrIndex= functionIndex.get(i);
+							if(aggrTypes.get(i)==0)
+							{
+								long longVal = childTuple.tuple.get(aggrIndex).toLong();
+								aggrValues.set(i, longVal);
+							}
+							else
+							{
+								double doubleVal = childTuple.tuple.get(aggrIndex).toDouble();
+								aggrValues.set(i, doubleVal);
+							}
+						}
+						retAggr.addAll(aggrValues);
+						hashAggrValues.put(groupByColVals, retAggr);
 					}
 		
 					groupByColVals="";
+					childTuple = leftChild.retNext();
 				}
 				init=1;
 				hashItr = hashAggr.keySet().iterator();
-				
-				if(parent instanceof OrderBy2)
-				{
-					OrderBy2 p = (OrderBy2)parent;
-					String keyVal = "";
-					long avgCnt = 0;
-					Tuple retTuple;
-					PrimitiveValue val;
-					while(hashItr.hasNext())
-					{
-						keyVal = hashItr.next();
-						avgCnt = aggrKeyCnt.get(keyVal);
-						
-						retTuple = hashAggr.get(keyVal);
-						for(int i=0;i<aggrFunctions.size();i++)
-						{
-							if(aggrFunctions.get(i).getName().equalsIgnoreCase("avg"))
-							{
-								val = retTuple.tuple.get(functionIndex.get(i));
-								Expression divide = new Division(val,new LongValue(avgCnt));
-								retTuple.tuple.set(functionIndex.get(i), eval.eval(divide)) ;
-							}
-							
-						}
-						p.otable.add(new ArrayList<PrimitiveValue>());
-						for(int i = 0; i < retTuple.tuple.size(); i++)
-						{
-							 p.otable.get(p.tai).add(retTuple.tuple.get(i));  
-							 
-						}
-						p.tai++;
-					}
-					
-					return null;
-				}
 				
 				
 			}
@@ -456,19 +478,32 @@ public class Aggregate2 extends RelationalAlgebra2 {
 			int avgCnt=0;
 			Tuple retTuple;
 			PrimitiveValue val;
+			List<Object> retAggr;
 			while(hashItr.hasNext())
 			{
 				keyVal = hashItr.next();
 				avgCnt = aggrKeyCnt.get(keyVal);
+				retAggr = hashAggrValues.get(keyVal);
+				
 				
 				retTuple = hashAggr.get(keyVal);
 				for(int i=0;i<aggrFunctions.size();i++)
 				{
-					if(aggrFunctions.get(i).getName().equalsIgnoreCase("avg"))
+					/*if(aggrFunctions.get(i).getName().equalsIgnoreCase("avg"))
 					{
 						val = retTuple.tuple.get(functionIndex.get(i));
 						Expression divide = new Division(val,new LongValue(avgCnt));
 						retTuple.tuple.set(functionIndex.get(i), eval.eval(divide)) ;
+					}
+					*/
+					
+					if(aggrTypes.get(i)==0)
+					{
+						retTuple.tuple.set(functionIndex.get(i), new LongValue((long) retAggr.get(i))) ;
+					}
+					else
+					{
+						retTuple.tuple.set(functionIndex.get(i), new DoubleValue((double) retAggr.get(i))) ;
 					}
 					
 				}
@@ -568,6 +603,84 @@ public class Aggregate2 extends RelationalAlgebra2 {
 		
 	}
 	
+	PrimitiveValue getSumAggr(List<Tuple> allTuples,Integer aggrIndex) throws SQLException
+	{
+		Eval eval = new Eval() {
+			public PrimitiveValue eval(Column arg0) throws SQLException {
+				return null;
+			}
+
+		};
+		
+		Iterator<Tuple> tupleItr = allTuples.iterator();
+		PrimitiveValue retLong = new LongValue(0);
+		PrimitiveValue retDouble = new DoubleValue(0.0);
+		int retType=-1; // 0 for long and 1 for double
+		while(tupleItr.hasNext())
+		{
+			PrimitiveValue val = tupleItr.next().tuple.get(aggrIndex);
+			if(val instanceof LongValue)
+			{
+				Expression add = new Addition(retLong,val);
+				retLong = eval.eval(add);
+				
+				retType=0;
+			}
+			else if(val instanceof DoubleValue)
+			{
+				Expression add = new Addition(retDouble,val);
+				retDouble = eval.eval(add);
+				
+				retType=1;
+			}
+			
+		}
+		
+		if(retType==0)
+		{
+			return retLong;
+		}
+		else
+		{
+			return retDouble;
+		}
+		
+	}
+	PrimitiveValue getAvgAggr(List<Tuple> allTuples,Integer aggrIndex) throws InvalidPrimitive
+	{
+		Iterator<Tuple> tupleItr = allTuples.iterator();
+		long longSum=0;
+		double doubleSum=0.0; 
+		int retType=-1; // 0 for long and 1 for double
+		int cnt = allTuples.size();
+		while(tupleItr.hasNext())
+		{
+			PrimitiveValue val = tupleItr.next().tuple.get(aggrIndex);
+			if(val instanceof LongValue)
+			{
+				longSum=longSum+val.toLong();
+				retType=0;
+			}
+			else if(val instanceof DoubleValue)
+			{
+				doubleSum = doubleSum+val.toDouble();
+				retType=1;
+			}
+			
+		}
+		
+		if(retType==0)
+		{
+			PrimitiveValue d = new LongValue(longSum/cnt);
+			return d;
+		}
+		else
+		{
+			PrimitiveValue d = new DoubleValue(doubleSum/cnt);
+			return d;
+		}
+		
+	}
 	
 	void computeAllStreamAggr(Tuple aggrTuple, Tuple nextTuple) throws SQLException //computes sum and avg functions
 	{
