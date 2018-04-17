529df1b369c047af2eab16a69f18f73e1c09cc74
DD <dhairyah@buffalo.edu>
2018-04-16 23:40:02 -0400

    Treemap in main condition

--- src/edu/buffalo/www/cse4562/Main.java
+++ src/edu/buffalo/www/cse4562/Main.java
@@ -335,7 +335,9 @@ import net.sf.jsqlparser.statement.select.SubSelect;
 		RelationalAlgebra2 parent=null;
 		
 		List<OrderByElement> orderbyEl = query.getOrderByElements();
-		if(orderbyEl != null || orderbyEl.get(0).toString().equals("L_LINESTATUS")) {
+		if(orderbyEl != null) {
+			if(!orderbyEl.get(0).toString().equals("L_LINESTATUS"))
+			{
 			RelationalAlgebra2 op = new OrderBy2();
 			OrderBy2 op1= (OrderBy2)op;
 			op1.element = orderbyEl;
@@ -346,6 +348,7 @@ import net.sf.jsqlparser.statement.select.SubSelect;
 			root = op;
 			parent = op;
 			rootCreated = 1;
+			}
 		}
 		
 		int aggr = 0; 
